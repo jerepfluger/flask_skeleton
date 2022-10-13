dbc5a1faaf6d162c746fc3204baa97abd565ef86
Jeremias Pfluger <jeremiaspfluger@Jeremias-Pfluger.local>
2022-10-13 11:44:17 +0200

    adding global exception handler

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,19 @@
+from werkzeug.exceptions import HTTPException
 from flask import Blueprint
 
 routes = Blueprint('routes', __name__)
 
 from .basic_controller import *
+
+
+@routes.errorhandler(HTTPException)
+def exception_handler(ex):
+    response = ex.get_response()
+    response.data = json.dumps({
+        'code': ex.code,
+        'name': ex.name,
+        'description': ex.description,
+    })
+    response.content_type = 'application/json'
+
+    return response
